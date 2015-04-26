dd07880f20ac42dce1ea38b80ff5d508d7c9a2bb
David da Silva <dasilvacontin@gmail.com>
2015-04-27 00:46:00 +0200

    initial code commit

---
+++ index.js
@@ -0,0 +1,242 @@
+function ListNode (value) {
+  this.clear()
+  this.value = value
+}
+ListNode.prototype.clear = function () {
+  this.prev = undefined
+  this.next = undefined
+  this.value = undefined
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols
+function ListIterator (node, reverse) {
+  if (!(node instanceof ListNode)) {
+    throw new Error('Expecting instance of ListNode as first argument')
+  }
+  reverse = !!reverse
+
+  var initialNode = {next: node, prev: node, value: undefined}
+  node = this._node = initialNode
+
+  /**
+   * Step iterator
+   */
+  this.next = function () {
+    this._node = node = node && (reverse && node.prev || node.next)
+    return this.current()
+  }
+
+  /**
+   * Return current element
+   */
+  this.current = function () {
+    this.value = node && node.value
+    this.done = !node
+    return node ?
+      {value: this.value, done: this.done} :
+      {done: this.done}
+  }
+
+  this.stepped = function () {
+    return node !== initialNode
+  }
+}
+
+function List () {
+  this.length = 0
+  this.first = undefined
+  this.last = undefined
+}
+exports.List = List
+
+/**
+ * Return iterator that iterates list from begin to end
+ **/
+List.prototype.Iterator = function () {
+  return new ListIterator(this.first)
+}
+
+/**
+ * Return iterator that iterates list from end to begin
+ **/
+List.prototype.ReverseIterator = function () {
+  return new ListIterator(this.last, true)
+}
+
+/**
+ * Test whether container is empty
+ **/
+List.prototype.empty = function () {
+  return !this.length
+}
+
+/**
+ * Access first element
+ **/
+List.prototype.front = function () {
+  if (this.first) {
+    return this.first.value
+  }
+}
+
+/**
+ * Access last element
+ **/
+List.prototype.back = function () {
+  if (this.last) {
+    return this.last.value
+  }
+}
+
+/**
+ * Assign new content to container
+ **/
+List.prototype.assign = function (arr) {
+  if (Array.isArray(arr)) {
+    this.clear()
+    var self = this
+    arr.forEach(function (val) {
+      self.push(val)
+    })
+  }
+}
+
+/**
+ * Deletes node from list and updates internal first/last references
+ **/
+List.prototype.popNode = function (node) {
+  if (node.prev) {
+    node.prev.next = node.next
+  }
+  if (node.next) {
+    node.next.prev = node.prev
+  }
+
+  if (this.first === node) {
+    this.first = node.next
+  }
+  if (this.last === node) {
+    this.last = node.prev
+  }
+  --this.length
+
+  node.clear()
+
+  return this
+}
+
+/**
+ * Insert element at beginning
+ **/
+List.prototype.pushFront = function (value) {
+  var node = new ListNode(value)
+  node.next = this.first
+  if (this.first) {
+    this.first.prev = node
+  }
+  this.first = node
+  if (!this.last) {
+    this.last = node
+  }
+  ++this.length
+  return this
+}
+
+/**
+ * Delete first element and return it
+ **/
+List.prototype.popFront = function () {
+  if (!this.first) {
+    return undefined
+  }
+  var node = this.first
+  var value = node.value
+  this.popNode(node)
+  return value
+}
+
+/**
+ * Add element at the end
+ **/
+List.prototype.push = function (value) {
+  var node = new ListNode(value)
+  node.prev = this.last
+  if (this.last) {
+    this.last.next = node
+  }
+  this.last = node
+  if (!this.first) {
+    this.first = node
+  }
+  ++this.length
+  return this
+}
+List.prototype.pushBack = List.prototype.push
+
+/**
+ * Delete last element and return it
+ **/
+List.prototype.pop = function () {
+  if (!this.last) {
+    return undefined
+  }
+  var node = this.last
+  var value = node.value
+  this.popNode(node)
+  return value
+}
+List.prototype.popBack = List.prototype.pop
+
+List.prototype.insert = function () {
+  throw new Error('Unimplemented')
+}
+
+/**
+ * Delete current element from list
+ **/
+List.prototype.erase = function (it) {
+  if (!(it instanceof ListIterator)) {
+    throw new Error('Expected ListIterator as first argument')
+  }
+  if (!it.stepped()) {
+    throw new Error('Iterator hasn\'t stepped into the list yet')
+  }
+  var node = it._node
+  if (!node) {
+    throw new Error('Iterator is done')
+  }
+
+  it.next()
+  var value = node.value
+  this.popNode(node)
+  return value
+}
+
+/**
+ * Clear content
+ */
+List.prototype.clear = function () {
+  var node = this.first
+  while (node) {
+    var nextNode = node.next
+    node.clear()
+    node = nextNode
+  }
+  this.first = undefined
+  this.last = undefined
+  this.length = 0
+}
+
+/**
+ * Clone list
+ **/
+List.prototype.clone = function () {
+  var list = new List()
+  var it = this.Iterator()
+  var item = it.next()
+  while (!item.done) {
+    list.push(item.value)
+    it.next()
+  }
+  return list
+}
---
+++ test/lijst.spec.js
@@ -0,0 +1,209 @@
+/* global describe, it, xit, beforeEach */
+var chai = require('chai')
+var should = chai.should()
+
+var lijst = require('../index.js')
+var List = lijst.List
+
+var list
+
+describe('lijst', function () {
+  beforeEach(function () {
+    list = new List()
+  })
+  describe('Capacity', function () {
+    it('#empty()', function () {
+      list.empty().should.be.true
+
+      list.push('something')
+      list.empty().should.be.false
+
+      list.pop()
+      list.empty().should.be.true
+    })
+
+    it('#length', function () {
+      list.length.should.equal(0)
+
+      list.push('something')
+      list.length.should.equal(1)
+
+      list.push('else')
+      list.length.should.equal(2)
+
+      list.pop()
+      list.pop()
+      list.length.should.equal(0)
+    })
+  })
+
+  describe('Element access', function () {
+    it('#front()', function () {
+      should.not.exist(list.front())
+
+      var val = 42
+      list.push(val)
+      list.front().should.equal(val)
+
+      val = 21
+      list.pushFront(21)
+      list.front().should.equal(val)
+
+      list.pop()
+      list.pop()
+      should.not.exist(list.front())
+    })
+
+    it('#back()', function () {
+      should.not.exist(list.back())
+
+      var val = 42
+      list.pushFront(val)
+      list.back().should.equal(val)
+
+      val = 21
+      list.push(21)
+      list.back().should.equal(val)
+
+      list.pop()
+      list.pop()
+      should.not.exist(list.back())
+    })
+  })
+  describe('Modifiers', function () {
+    it('#assign()', function () {
+      var arr = [1, 3, 5]
+      list.assign([1, 3, 5])
+      list.length.should.equal(3)
+
+      arr.forEach(function (val) {
+        list.front().should.equal(val)
+        list.popFront()
+      })
+    })
+
+    it('#pushFront()', function () {
+      var val = 'test'
+      list.pushFront(val)
+      list.front().should.equal(val)
+      list.back().should.equal(val)
+      list.length.should.equal(1)
+
+      var val2 = 'testerino'
+      list.pushFront(val2)
+      list.front().should.equal(val2)
+      list.back().should.equal(val)
+      list.length.should.equal(2)
+    })
+
+    it('#popFront()', function () {
+      var val = 523
+      var val2 = 29367
+      list.push(val)
+      list.push(val2)
+
+      list.length.should.equal(2)
+      list.popFront().should.equal(val)
+      list.length.should.equal(1)
+      list.front().should.equal(val2)
+      list.back().should.equal(val2)
+      list.popFront().should.equal(val2)
+      list.length.should.equal(0)
+      should.not.exist(list.front())
+      should.not.exist(list.back())
+      should.not.exist(list.popFront())
+    })
+
+    it('#push() #pushBack()', function () {
+      var val = 2876
+      list.push(val)
+      list.length.should.equal(1)
+      list.front().should.equal(val)
+      list.back().should.equal(val)
+
+      var val2 = 351
+      list.push(val2)
+      list.length.should.equal(2)
+      list.front().should.equal(val)
+      list.back().should.equal(val2)
+
+      var val3 = 152
+      list.push(val3)
+      list.length.should.equal(3)
+      list.front().should.equal(val)
+      list.back().should.equal(val3)
+    })
+
+    it('#pop() #popBack()', function () {
+      var arr = [2235, 347, 458]
+      list.assign(arr)
+      while (arr.length) {
+        list.pop().should.equal(arr.pop())
+        list.length.should.equal(arr.length)
+        if (list.length) {
+          list.front().should.equal(arr[0])
+          list.back().should.equal(arr[arr.length - 1])
+        } else {
+          should.not.exist(list.front())
+          should.not.exist(list.back())
+        }
+      }
+    })
+
+    xit('#insert()')
+
+    it('#erase()', function () {
+      var arr = [1, 2, 3, 4, 5, 6]
+      var DELETE = 'delete'
+      var SKIP = 'skip'
+      var actions = [DELETE, SKIP, DELETE, SKIP, DELETE, DELETE]
+      list.assign(arr)
+      var it = list.Iterator()
+      var arrIt = 0
+      it.next() // now at `1`
+
+      while (actions.length) {
+        var action = actions.shift()
+        if (action === DELETE) {
+          list.erase(it).should.equal(arr.splice(arrIt, 1)[0])
+          list.length.should.equal(arr.length)
+          list.front().should.equal(arr[0])
+          list.back().should.equal(arr[arr.length - 1])
+        } else if (action === SKIP) {
+          ++arrIt
+          it.next()
+        }
+      }
+
+      it.current().done.should.be.true
+    })
+
+    it('#swap()')
+
+    it('#resize()')
+
+    it('#clear()', function () {
+      list.assign([1, 2, 3])
+      list.clear()
+      should.not.exist(list.front())
+      should.not.exist(list.back())
+      list.length.should.equal(0)
+    })
+  })
+
+  describe('Operations', function () {
+    it('#splice()')
+
+    it('#remove()')
+
+    it('#removeIf()')
+
+    it('#unique()')
+
+    it('#merge()')
+
+    it('#sort()')
+
+    it('#reverse()')
+  })
+})
